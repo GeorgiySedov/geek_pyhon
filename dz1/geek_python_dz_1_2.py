c66f88d94035b63fa9c0b8b7366bf9996f5baa03
Georgiy Sedov <xzeto@mail.ru>
2021-11-04 17:19:19 +0300

    Изменен geek_python_1_2.py

--- dz1/geek_python_dz_1_2.py
+++ dz1/geek_python_dz_1_2.py
@@ -1,20 +1,12 @@
 seconds_input = int(input ("Введите время в секундах >>> "))
 
-#Считаем часы, если секунд >=3600
-if seconds_input >= 3600:
-    hours = seconds_input//3600
-else:
-    hours = 0
-# Считаем минуты, если секунд > 60, (от остатка деления на 3600)
-if seconds_input > 60:
-    minutes = (seconds_input % 3600)//60
-else:
-    minutes = 0
+#Считаем часы
+hours = seconds_input//3600
 
-# Считаем секунды, если их больше 0, (от остатка деления на 60)
-if seconds_input > 0:
-    seconds = (seconds_input % 60)
-else:
-    seconds = 0
+# Считаем минуты
+minutes = (seconds_input % 3600)//60
+
+# Считаем секунды
+seconds = (seconds_input % 60)
 
 print ("\n",hours,":",minutes,":",seconds)
